c6a9962903d4abece3ba5d138070b73d6049f706
Michele Cavazzutti <Miky.cava@gmail.com>
2021-03-16 08:28:03 +0000

    Typos Print

--- src/Inference/Include/Wald_Solver_imp.h
+++ src/Inference/Include/Wald_Solver_imp.h
@@ -229,7 +229,7 @@ MatrixXv Wald_Solver<InputHandler>::compute_inference_output(void){
   template<typename InputHandler>
   void Wald_Solver<InputHandler>::print_for_debug(void) const {
 
-  std::cout << "S computed: "is_S_computed << std::endl; 
+  std::cout << "S computed: " << is_S_computed << std::endl; 
   if(is_S_computed==true){
   std::cout << "Matrix Smoothing S is (only some samples): \n" << std::endl;
   for (UInt i=0; i<10; i++){
@@ -239,7 +239,7 @@ MatrixXv Wald_Solver<InputHandler>::compute_inference_output(void){
   for (UInt i=0; i<10; i++){
   std::cout << "St(" << 10*i << "," << 20*i << "): " << St(10*i,20*i) << std::endl;
   } 
-  std::cout << "V computed: "is_V_computed << std::endl; 
+  std::cout << "V computed: " << is_V_computed << std::endl; 
   if(is_S_computed==true){
   std::cout << "Matrix variance V is: \n" << S << std::endl;
   std::cout << "Matrix variance transpose Vt is: \n" << St << std::endl;
