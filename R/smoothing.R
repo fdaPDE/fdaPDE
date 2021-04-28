f660ac08e94cd169f5bd947ec6da2ebf71b91549
Cristina Galimberti <cristina2.galimberti@mail.polimi.it>
2021-04-28 08:41:43 +0000

    Computation of pvalues moved in R

--- R/smoothing.R
+++ R/smoothing.R
@@ -901,17 +901,32 @@ smooth.FEM<-function(locations = NULL, observations, FEMbasis,
     fit.FEM  = FEM(solution$f, FEMbasis)
     PDEmisfit.FEM = FEM(solution$g, FEMbasis)
 
-    # Save p_values and intervals
+    # Save statistics and intervals
     if(R_Inference_Data_Object@definition==1){
       if(R_Inference_Data_Object@test==0){
         confidence_Intervals=t(matrix(data=bigsol[[24]],nrow = 3,ncol =dim(R_Inference_Data_Object@coeff)[1]))
         inference=list(CI=confidence_Intervals)
       }else{
-        if(R_Inference_Data_Object@interval==0){
-          p_values=bigsol[[23]]
-          inference=list(p_vals=p_values)
+        statistics=bigsol[[23]]
+        if(R_Inference_Data_Object@type==3){
+          p_values = statistics
         }else{
-          p_values=bigsol[[23]]
+          # Compute p-values
+          if(R_Inference_Data_Object@test==1){
+            # one-at-the-time-tests
+            p_values = numeric(length(statistics))
+            for(i in 1:length(statistics)){
+              p_values[i] = 2*pnorm(-abs(statistics[i]))
+            }
+          }
+          else{
+            # simultaneous tests
+            p_values = 2*pnorm(-abs(statistics[1]))
+          }
+        }
+        if(R_Inference_Data_Object@interval==0){
+            inference=list(p_vals=p_values)
+        }else{
           confidence_Intervals=t(matrix(data=bigsol[[24]],nrow = 3,ncol =dim(R_Inference_Data_Object@coeff)[1]))
           inference=list(p_vals=p_values, CI=confidence_Intervals)
         }
