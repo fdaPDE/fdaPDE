8cf18bf586b1ac72cb0edd9c8e5525f969346477
Giulia Ferla <62119243+ferlagiulia@users.noreply.github.com>
2021-08-03 15:05:07 +0200

    Updated time test (the new library returns only the optimal value of beta)

--- tests/smooth.FEM.time.tests.R
+++ tests/smooth.FEM.time.tests.R
@@ -192,7 +192,7 @@ output_CPP<-smooth.FEM.time(locations = locations, time_mesh = TimePoints,
 bestlambdas = which(output_CPP$GCV == min(output_CPP$GCV), arr.ind = TRUE)
 plot(FEM.time(output_CPP$fit.FEM$coeff[,bestlambdas[1],bestlambdas[2]],FEMbasis = FEMbasis, time_mesh = TimePoints), t=1)
 
-output_CPP$beta[,bestlambdas[1],bestlambdas[2]]
+output_CPP$beta
 
 
 
