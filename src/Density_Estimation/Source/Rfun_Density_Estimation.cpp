0b4aaedfaa54511e1cff5c5c2c68fc74ed89ef7c
aldoclemente <aldo.clemente@mail.polimi.it>
2021-10-31 19:02:24 +0100

    Density initialization for graph domains.

--- src/Density_Estimation/Source/Rfun_Density_Estimation.cpp
+++ src/Density_Estimation/Source/Rfun_Density_Estimation.cpp
@@ -99,7 +99,12 @@ extern "C" {
         		return(DE_init_skeleton<1, 3, 3>(Rdata, Rorder, Rfvec, RheatStep, RheatIter, Rlambda, Rnfolds, Rnsim, RstepProposals, Rtol1, Rtol2, Rprint, Rmesh, Rsearch, init, init_fold));
             else if(order == 2 && mydim==3 && ndim==3)
                 return(DE_init_skeleton<2, 3, 3>(Rdata, Rorder, Rfvec, RheatStep, RheatIter, Rlambda, Rnfolds, Rnsim, RstepProposals, Rtol1, Rtol2, Rprint, Rmesh, Rsearch, init, init_fold));
+            else if(order == 1 && mydim==1 && ndim==2)
+                return(DE_init_skeleton<1, 1, 2>(Rdata, Rorder, Rfvec, RheatStep, RheatIter, Rlambda, Rnfolds, Rnsim, RstepProposals, Rtol1, Rtol2, Rprint, Rmesh, Rsearch, init, init_fold));
+            else if(order == 2 && mydim==1 && ndim==2)
+                return(DE_init_skeleton<2, 1, 2>(Rdata, Rorder, Rfvec, RheatStep, RheatIter, Rlambda, Rnfolds, Rnsim, RstepProposals, Rtol1, Rtol2, Rprint, Rmesh, Rsearch, init, init_fold));
 
-        	return(NILSXP);
+
+            return(NILSXP);
         }
 }
